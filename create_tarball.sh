c84eab89be380989aad785970341331ca0e4d7fe
Haikel Guemar <hguemar@fedoraproject.org>
2017-01-20 01:52:34 +0100

    Add create_tarball script
    Simplify packaging by generating tarballs with a simple script.
    ./create_tarball.sh should retrieve info from spec and generate
    the tarball for you.

---
+++ create_tarball.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+set -ex
+
+REPONAME="tripleo-ui"
+REPO="https://github.com/openstack/$REPONAME"
+COMMIT=`awk '/%global commit/ { print $3 }' *spec`
+SHORT_COMMIT=${COMMIT:0:7}
+TARBALL="tripleo-ui-deps-$SHORT_COMMIT.tar.gz"
+
+function generate_archive() {
+    rm -rf $REPONAME
+    git clone $REPO
+    pushd $REPONAME
+        git checkout $SHORT_COMMIT
+        npm install
+        tar czf $TARBALL node_modules
+        mv $TARBALL ..
+    popd
+}
+
+generate_archive
