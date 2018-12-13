8f87335c3ef2e3150a6f46932f959284b0733c65
Alan Pevec <alan.pevec@redhat.com>
2018-12-13 03:38:45 +0100

    run npm install in a centos container
    to avoid incompatible local npm versions
    TODO move to buildah + podman

--- create_tarball.sh
+++ create_tarball.sh
@@ -12,10 +12,22 @@ TARBALL="tripleo-ui-deps-$REVIEW.$PATCHSET.tar.gz"
 function generate_archive() {
     rm -rf $REPONAME
     git clone $REPO
+    cat <<-EOF > Dockerfile
+    FROM centos:centos7
+    RUN curl -o /etc/yum.repos.d/deps.repo https://trunk.rdoproject.org/centos7-rocky/delorean-deps.repo
+    RUN yum -y install npm
+    VOLUME ["/src"]
+    WORKDIR /src
+    CMD ["npm","install"]
+    EOF
     pushd $REPONAME
         git fetch origin refs/changes/$REVIEW2/$REVIEW/$PATCHSET
         git checkout FETCH_HEAD
-        npm install
+        sudo docker build -t ooouideps ..
+        # npm install in a centos7 container
+        # FIXME with SELinux enforcing fails with
+        # Error: EACCES: permission denied, scandir '/src'
+        sudo docker run -v $PWD:/src ooouideps
         tar czf $TARBALL node_modules
         mv $TARBALL ..
     popd
