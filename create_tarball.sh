c52447a6474c9163c4f6cb2f32fff00864bde5ad
Honza Pokorny <honza@redhat.com>
2018-12-12 20:41:42 -0400

    Add an npm version check to build script

--- create_tarball.sh
+++ create_tarball.sh
@@ -8,6 +8,8 @@ REVIEW=$(awk '/%global review/ { print $3 }' *spec)
 REVIEW2=$(awk '/%global review/ { print substr($3,length($3)-1,2)}' *spec)
 PATCHSET=$(awk '/%global patchset/ { print $3 }' *spec)
 TARBALL="tripleo-ui-deps-$REVIEW.$PATCHSET.tar.gz"
+NPM_VERSION=$(npm --version)
+REQUIRED_NPM_VERSION="5.6.0"
 
 function generate_archive() {
     rm -rf $REPONAME
@@ -21,4 +23,14 @@ function generate_archive() {
     popd
 }
 
+function assert_correct_npm_version() {
+    if [ $NPM_VERSION != $REQUIRED_NPM_VERSION ]; then
+        echo 'ERROR: Wrong version of npm detected.'
+        echo "ERROR: version required $REQUIRED_NPM_VERSION"
+        echo "ERROR: actual version $NPM_VERSION"
+        exit 1;
+    fi
+}
+
+assert_correct_npm_version
 generate_archive
