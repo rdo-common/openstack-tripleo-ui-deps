7270a1f194c440e9cc003378ed17a6b4c94bd2e7
Haikel Guemar <hguemar@fedoraproject.org>
2018-12-17 16:53:22 +0100

    Iterate over dockerized tarball creationg
    - Set SELinux context so we can write in mounted volume within container
    - Run the tarball creation as a non-privileged user
    - Simplify a bit

--- create_tarball.sh
+++ create_tarball.sh
@@ -8,29 +8,29 @@ REVIEW=$(awk '/%global review/ { print $3 }' *spec)
 REVIEW2=$(awk '/%global review/ { print substr($3,length($3)-1,2)}' *spec)
 PATCHSET=$(awk '/%global patchset/ { print $3 }' *spec)
 TARBALL="tripleo-ui-deps-$REVIEW.$PATCHSET.tar.gz"
+USER_ID=$(id -u)
+
 
 function generate_archive() {
     rm -rf $REPONAME
     git clone $REPO
     cat <<-EOF > Dockerfile
-    FROM centos:centos7
-    RUN curl -o /etc/yum.repos.d/deps.repo https://trunk.rdoproject.org/centos7-rocky/delorean-deps.repo
-    RUN yum -y install npm
-    VOLUME ["/src"]
-    WORKDIR /src
-    CMD ["npm","install"]
-    EOF
-    pushd $REPONAME
-        git fetch origin refs/changes/$REVIEW2/$REVIEW/$PATCHSET
-        git checkout FETCH_HEAD
-        sudo docker build -t ooouideps ..
-        # npm install in a centos7 container
-        # FIXME with SELinux enforcing fails with
-        # Error: EACCES: permission denied, scandir '/src'
-        sudo docker run -v $PWD:/src ooouideps
-        tar czf $TARBALL node_modules
-        mv $TARBALL ..
-    popd
+FROM centos:centos7
+RUN curl -o /etc/yum.repos.d/deps.repo https://trunk.rdoproject.org/centos7-rocky/delorean-deps.repo
+RUN yum -y install npm
+WORKDIR /src
+RUN chown -R $USER_ID:0 /src  && chmod -R ug+rwX /src
+CMD npm install && tar czf $TARBALL node_modules
+USER $USER_ID
+EOF
+    # Configure cache settings not write outside our workdir
+    cp npmrc $PWD/$REPONAME/.npmrc
+    # Fix SELinux context so we can write in the mounted
+    # volume within the container
+    chcon -Rt svirt_sandbox_file_t $PWD/$REPONAME
+    sudo docker build -t ooouideps .
+    sudo docker run -v $PWD/$REPONAME:/src ooouideps
+    mv $PWD/$REPONAME/$TARBALL .
 }
 
 generate_archive
